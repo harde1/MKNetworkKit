b2de3d71cb81e8a28ebc5efb19598a26c32571c4
harde1 <harde1@163.com>
2015-07-20 11:43:29 +0800

    111
    1111

--- MKNetworkKit/MKNetworkEngine.m
+++ MKNetworkKit/MKNetworkEngine.m
@@ -43,7 +43,7 @@
 @interface MKNetworkEngine (/*Private Methods*/)
 
 @property (copy, nonatomic) NSString *hostName;
-@property (strong, nonatomic) Reachability *reachability;
+@property (strong, nonatomic) MyReachability *reachability;
 @property (copy, nonatomic) NSDictionary *customHeaders;
 @property (assign, nonatomic) Class customOperationSubclass;
 
@@ -115,7 +115,7 @@ static NSOperationQueue *_sharedNetworkQueue;
                                                  object:nil];
       
       self.hostName = hostName;
-      self.reachability = [Reachability reachabilityWithHostname:self.hostName];
+      self.reachability = [MyReachability reachabilityWithHostname:self.hostName];
       
       dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
